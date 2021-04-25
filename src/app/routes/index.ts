e5a2a5cad8421fa764b7ca243008f10dc6099641
loricode <loritec26@gmail.com>
2021-04-25 16:15:16 -0500

    segundo commit

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -13,12 +13,39 @@ router.get('/estudiantes', function(req, res){
     });
 });
 
-router.get('/estudiantes', function(req, res){
+router.post('/estudiantes', function(req, res){
   const obj = req.body
   const query = "INSERT INTO estudiante SET ?";  
   db.query(query, obj, function(error, results, fields){
      res.json(results.insertId)
   })
 });
+
+router.delete('/estudiantes/:id', function(req, res){
+  const { id } = req.params
+  const query = "DELETE FROM estudiante WHERE id = ?"; 
+  db.query(query, id, function(error, results, fields){
+     res.json({"msg":"removed", results})
+  });
+});
+
+router.get('/estudiantes/:id', function(req, res){
+  const { id } = req.params
+  const query = "SELECT * FROM estudiante WHERE id = ?"; 
+  db.query(query, id, function(error, results, fields){
+     res.json({"msg":"estudiante", results})
+  });
+})
+
+router.put('/estudiantes', function(req, res){
+  const { id, nombre, edad, telefono } = req.body
+  const query = "UPDATE estudiante SET nombre = ?, edad = ?,"+
+                "telefono = ? WHERE id = ?"
+  let arryData = [nombre, edad, telefono, id]          
+  db.query(query, arryData, function(error, results, fields){
+     res.json({"msg":"updated", results})
+  });
+})
+
 
 module.exports =  router;
